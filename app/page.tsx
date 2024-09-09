647b3cb06d23a7318d019c4bdcb60056e7a170bf
EXSA DEV <exsadev@EXSAs-iMac.local>
2024-09-09 13:09:33 +0700

    shit, im fogot use semantic

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <h1>I'm not in a good mood rn.</h1>
+      <h1>I&apos;m not in a good mood rn.</h1>
     </main>
   );
 }
